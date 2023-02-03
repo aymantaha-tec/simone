4692321dddb2f20e1727fefa88fdefbe730cfbb4
AYMAN taha <62824216+ayta88@users.noreply.github.com>
2023-02-03 23:23:15 +0200

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,22 @@
+
+// set the title attribute for an HTML element dynamically by code javascript.
+let list = document.querySelectorAll("ul li a");
+list.forEach( function(ele) {
+    let sValue = ele.innerHTML;
+    ele.setAttribute("title", sValue);
+
+    // Add behavior Smooth 
+    ele.addEventListener("click", clickHandler);
+        function clickHandler(e) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+        document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+        });
+    };
+});
+
+
+
+
+
